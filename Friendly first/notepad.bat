7298cb3a5431bdbc150a6e3e6626e1df38a9545d
Nihal Awasthi <137594290+nihalawasthi@users.noreply.github.com>
2024-05-20 22:08:17 +0530

    Update notepad.bat

--- Friendly first/notepad.bat
+++ Friendly first/notepad.bat
@@ -1,7 +1,5 @@
 @echo off
 powershell -Command "notepad"
-cd %TEMP%
-Powershell -Command "Invoke-WebRequest 'https://raw.githubusercontent.com/nihalawasthi/Trojans/main/Friendly%20first/prank.txt' -OutFile prank.txt"
 while :
 do
 	echo "You have been Hacked"
